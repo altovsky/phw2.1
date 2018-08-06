c216ebf26ac054a37f60224425ec25bcf727d115
altovsky <41269058+altovsky@users.noreply.github.com>
2018-08-06 09:02:27 +0300

    mod

--- dz2.1.py
+++ dz2.1.py
@@ -3,44 +3,28 @@
 from pprint import pprint
 
 
-def count_file_lines(file_path):
-    fl = open(file_path, mode='r', encoding='utf-8')
-    file_lines_number = len(fl.readlines())
-    fl.close()
-    return file_lines_number
-
-
 def read_cook_book(cook_book_path):
-    cb_lines_number = count_file_lines(cook_book_path)
     cook_book = dict()
     with open(cook_book_path, mode='r', encoding='utf-8') as db:
-        current_line = 0  # Счетчик прочитанных строк
         while True:
-            dish = db.readline()
-            cook_book[dish.strip()] = current_line
-            current_line += 1
-            # Далее не будем использовать, встроенный в данные счетчик ингридиентов. Посчитаем сами.
-            number_of_ingredients = db.readline()
-            current_line += 1
-            cook_book[dish.strip()] = []
+            dish = db.readline().strip()
+            cook_book[dish] = []
 
-            while True:
-                ingridient_record = db.readline()
-                ingridient_record = ingridient_record.strip()
-                current_line += 1
-                if ingridient_record == '':  # Если конец "секции"
-                    break
-                else:
-                    ingridient_data = ingridient_record.split(' | ')
-                    cook_book[dish.strip()].append(
-                        {
-                            'ingridient_name': ingridient_data[0],
-                            'quantity': int(ingridient_data[1]),
-                            'measure': ingridient_data[2]
-                        }
-                    )
+            number_of_ingredients = db.readline().strip()
+            for iteration in range(int(number_of_ingredients)):
+                ingridient_record = db.readline().strip()
+                ingridient_data = ingridient_record.split(' | ')
 
-            if current_line > cb_lines_number:  # Если последняя строка в файле
+                cook_book[dish].append(
+                    {
+                        'ingridient_name': ingridient_data[0],
+                        'quantity': int(ingridient_data[1]),
+                        'measure': ingridient_data[2]
+                    }
+                )
+
+            separator = db.readline()
+            if not separator:
                 break
 
     return cook_book
@@ -50,16 +34,15 @@ def get_shop_list_by_dishes(dishes, person_count, cook_book_file_path):
     cook_book_db = read_cook_book(cook_book_file_path)
     shop_list_by_dishes = {}
 
-    for i in dishes:
-        for ii in range(len(cook_book_db[i])):
-            ingridient_name = cook_book_db[i][ii]['ingridient_name']
+    for dish in dishes:
+        for ingredient in cook_book_db[dish]:
+            ingridient_name = ingredient['ingridient_name']
             if ingridient_name in shop_list_by_dishes:
-                shop_list_by_dishes[ingridient_name]['quantity'] += \
-                    cook_book_db[i][ii]['quantity'] * person_count
+                shop_list_by_dishes[ingridient_name]['quantity'] += ingredient['quantity'] * person_count
             else:
                 shop_list_by_dishes[ingridient_name] = {
-                    'measure': cook_book_db[i][ii]['measure'],
-                    'quantity': cook_book_db[i][ii]['quantity'] * person_count
+                    'measure': ingredient['measure'],
+                    'quantity': ingredient['quantity'] * person_count
                     }
 
     return shop_list_by_dishes
